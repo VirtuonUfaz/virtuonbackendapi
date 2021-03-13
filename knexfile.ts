532be00f298fff8e9af545fee49146856b4953d8
Anday <48630069+anday2000@users.noreply.github.com>
2021-03-13 12:08:28 +0400

    Update knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -1,38 +1,41 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 dotenv.config();
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
+const dbConfig = {
+  development:{
+    client: "postgresql",
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_DATABASE,
+    },
+    migrations: {
+      extension: "ts",
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      extension: "ts",
+      directory: __dirname + "/db/seeds",
+    },
+  },
+  production:{
+    client: "postgresql",
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_DATABASE,
+    },
+    migrations: {
+      extension: "ts",
+      directory: __dirname + "/db/migrations",
+    },
+    seeds: {
+      extension: "ts",
+      directory: __dirname + "/db/seeds",
+    },
+  }
+}
 
-export const development = {
-  client: "postgresql",
-  connection: {
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-  },
-  migrations: {
-    extension: "ts",
-    directory: __dirname + "/db/migrations",
-  },
-  seeds: {
-    extension: "ts",
-    directory: __dirname + "/db/seeds",
-  },
-};
-export const production = {
-  client: "postgresql",
-  connection: {
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-  },
-  migrations: {
-    extension: "ts",
-    directory: __dirname + "/db/migrations",
-  },
-  seeds: {
-    extension: "ts",
-    directory: __dirname + "/db/seeds",
-  },
-};
+module.exports = dbConfig;
