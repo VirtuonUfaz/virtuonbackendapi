029000832347426248d11d8f9985ffcfb4e3da22
Anday <48630069+anday2000@users.noreply.github.com>
2021-03-14 16:55:31 +0400

    Add auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+
+export default function (req, res, next) {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  // Verify token
+  try {
+    jwt.verify(token, process.env.JWT_SECRET || "" , (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: "Token is not valid" });
+      } else {
+        /* 
+        decoded.user returns id of the user in user object:  
+            user: {
+                id: user.id
+            }
+        */
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (err) {
+    console.error("something wrong with auth middleware");
+    res.status(500).json({ msg: "Server Error" });
+  }
+}
