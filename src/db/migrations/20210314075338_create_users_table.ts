ca6e2a497809d4114e2430e4e25fe8a97d03f457
Anar Mammad <anar.mammad28@gmail.com>
2021-03-14 12:24:42 +0400

    Add users migration file

---
+++ src/db/migrations/20210314075338_create_users_table.ts
@@ -0,0 +1,47 @@
+import { Knex } from "knex";
+
+/* 
+Table Users {
+    Id int [pk]
+    Username varchar [unique]
+    Firstname varchar
+    Lastname varchar
+    Gender char
+    PhoneNumber varchar [unique]
+    Email varchar [unique]
+    BirthDate timestamp
+    RegisterDate timestamp
+    AuthToken varchar
+    PhoneVerified boolean
+    EmailVerified boolean
+    IsBlocked boolean
+    ProfilePictureUrl varchar
+    Role int [ref: > Roles.Id]
+    Status enum //Online, Offline, Busy
+}
+*/
+
+export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('users', function(table) {
+        table.increments();
+        table.string('username').notNullable();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.enu('gender', ['Male', 'Female']).notNullable();
+        table.string('phone_number').notNullable();
+        table.string('email').notNullable();
+        table.timestamp('birth_date').notNullable();
+        table.string('auth_token');
+        table.boolean('phone_verified').notNullable().defaultTo(false);
+        table.boolean('email_verified').notNullable().defaultTo(false);
+        table.boolean('is_blocked').notNullable().defaultTo(false);
+        table.string('profile_picture_url');
+        table.integer('role_id'); // TODO: foreign key will be within separete migration
+        table.timestamps(); // created_at, updated_at
+    })
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('users');
+}
