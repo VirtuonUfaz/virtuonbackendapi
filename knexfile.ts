10b6450e62b2bdf5286be0231a2d761656fb7935
Anday <48630069+anday2000@users.noreply.github.com>
2021-03-14 11:14:40 +0400

    Fix watch

--- knexfile.ts
+++ knexfile.ts
@@ -38,4 +38,4 @@ const dbConfig = {
   }
 }
 
-module.exports = dbConfig;
+export = dbConfig;
--- src/db/connect.ts
+++ src/db/connect.ts
@@ -1,6 +1,6 @@
 import * as knex from "knex";
 import { attachPaginate } from "knex-paginate";
-import {development, production} from '../../knexfile'
-import {isDev} from '../helpers/environment'
+import { development, production } from "../../knexfile";
+import { isDev } from "../helpers/environment";
 attachPaginate();
 export default knex.knex(isDev() ? development : production);
