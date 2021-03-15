09bed1f4fe4e32acc3b57c6786086252e54e6799
ibrahimkhanlimurad <ibrahimxanlimurad@hotmail.com>
2021-03-15 20:57:04 +0400

    Update dockerfile

--- src/db/migr/10015_user_verification_codes.ts
+++ src/db/migr/10015_user_verification_codes.ts
@@ -2,7 +2,7 @@ import { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable('user_verification_codes', function(table) {
-        table.increments();
+        table.increments("id").primary();
         table
             .integer("user_id")
             .notNullable()
