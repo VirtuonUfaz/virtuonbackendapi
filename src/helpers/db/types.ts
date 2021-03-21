fb8af1ac2f834f8a7e8011fa93d7b08857789439
Nauboys <muhammad.samadzade@gmail.com>
2021-03-21 22:24:42 +0400

    types

--- src/helpers/db/types.ts
+++ src/helpers/db/types.ts
@@ -13,7 +13,18 @@ enum Years {
   L1 = "l1",
   L2 = "l2",
   L3 = "l3",
+  Master = 'master', 
+  Graduated = 'graduated'
 }
+enum Types{
+  Mid1 = 'mid1', 
+  Mid2 = 'mid2',
+  Project = 'project',
+  Pw = 'pw',
+  Final = 'final',
+  Retake = 'retake'
+}
+
 export interface UserType {
   id: number;
   user_name: String;
@@ -64,4 +75,181 @@ export interface VerificationCodeType {
   email_code: String;
   created_at: Date;
   generation_trials: number;
+}
+
+export interface ArchiveVideoLogsType {
+  id: number;
+  student_id: number;
+  video_id: number;
+  stayed_at: Date;
+  created_at: Date;
+  updated_at: Date;
+}
+
+
+export interface CoursesType {
+  id: number;
+  code: String;
+  key: String;
+  value: String;
+  coefficient: number;
+  head_teacher_id: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+
+export interface EventClasseType {
+  event_id: number;
+  teacher_id: number;
+  subject_id: number;
+  group_id: number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+
+export interface EventExtrasType {
+  event_id: number;
+  name: String;
+  host: String;
+  participant_number: number;
+  is_public: Boolean;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface EventMessagesType {
+  id: number;
+  user_id: number;
+  body: String;
+  date: Date;
+  event_id: number;
+  file: String;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface EventRegistersType {
+  id: number;
+  user_id: number;
+  full_name: String;
+  email: String;
+  phone_number: String;
+  is_participated: Boolean;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface EventTypesType {
+  id: number;
+  name: String;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface EventRegistersType {
+  id: number;
+  event_type_id: number;
+  room_id: number;
+  start_date: Date;
+  end_date: Date;
+  is_online: Boolean;
+  event_link:String;
+  author_id:number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface ExamsType {
+  id: number;
+  type: Types;
+  coefficient: number;
+  course_id: number;
+  event_id: number;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface GradesType {
+  id: number;
+  exam_id : number;
+  student_id: number;
+  grade: number;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface GroupCoursesType {
+  group_id: number;
+  course_id : number;
+  updated_at: Date;
+}
+export interface GroupsType {
+  id: number;
+  code : String;
+  name: String;
+  year: Years;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface HolidaysType {
+  id: number;
+  code : String;
+  start_date: Date;
+  end_date: Date;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface JCategoriesType {
+  id: number;
+  key : number;
+  value: String;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface JTagsType {
+  id: number;
+  key : number;
+  value: String;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface JournalCategoriesType {
+  journal_id  : number;
+  category_id : number;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export interface JournalTagsType {
+  journal_id  : number;
+  tag_id : number;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface JournalsType {
+  id: number;
+  author : number;
+  title: String;
+  description: String;
+  upload_date: Date;
+  publication_date : Date;
+  file: String;
+  journal: String;
+  publisher: String;
+  issue: number;
+  created_at: Date;
+  updated_at: Date;
+}
+export interface PrivateTasksType {
+  id: number;
+  author : number;
+  title: String;
+  description: String;
+  upload_date: Date;
+  publication_date : Date;
+  file: String;
+  journal: String;
+  publisher: String;
+  issue: number;
+  created_at: Date;
+  updated_at: Date;
 }
