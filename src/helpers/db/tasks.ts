1de6af4bdfb1ef09f0e01f0799089e0730945d30
Anday <48630069+anday2000@users.noreply.github.com>
2021-03-23 11:57:36 +0400

    :bug: Fix bug with tasks date

--- src/helpers/db/tasks.ts
+++ src/helpers/db/tasks.ts
@@ -5,17 +5,17 @@ import knex from "../../db/connect";
 export const get = async (
   userId: number
 ): Promise<Array<TypesDB.PrivateTasksType>> =>
-  await knex(TABLE_NAMES.PRIVATE_TASKS).select("*").where("user_id", userId);
+  await knex(TABLE_NAMES.PRIVATE_TASKS).select("*").where("user_id", userId).orderBy("created_at", "desc");
 
 export const create = async (
   task: Partial<TypesDB.PrivateTasksType>
 ): Promise<TypesDB.PrivateTasksType> =>
-  await knex(TABLE_NAMES.PRIVATE_TASKS).insert(task);
+  await knex(TABLE_NAMES.PRIVATE_TASKS).insert({...task, created_at: knex.fn.now()});
 
 export const update = async (
   task: Partial<TypesDB.PrivateTasksType>
 ): Promise<number> =>
-  await knex(TABLE_NAMES.PRIVATE_TASKS).update(task).where("id", task.id).andWhere("user_id",task.user_id);
+  await knex(TABLE_NAMES.PRIVATE_TASKS).update({...task, updated_at: knex.fn.now()}).where("id", task.id).andWhere("user_id",task.user_id);
 
 export const remove = async (
     task: Partial<TypesDB.PrivateTasksType>
